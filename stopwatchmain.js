68ee77a571c7230e5ef17cfcf84cb9b83faffb63
gvr-prakashreddy <gangireddyprakashreddy55@gmail.com>
2024-08-28 23:03:39 +0530

    Add files via upload

---
+++ stopwatchmain.js
@@ -0,0 +1,65 @@
+let startBtn = document.getElementById('start'); 
+let stopBtn = document.getElementById('stop'); 
+let resetBtn = document.getElementById('reset'); 
+
+let hour = 0; 
+let minute = 0; 
+let second = 0; 
+let count = 0; 
+let timer = false; // Declare the timer variable
+
+startBtn.addEventListener('click', function () { 
+	timer = true; 
+	stopWatch(); 
+}); 
+
+stopBtn.addEventListener('click', function () { 
+	timer = false; 
+}); 
+
+resetBtn.addEventListener('click', function () { 
+	timer = false; 
+	hour = 0; 
+	minute = 0; 
+	second = 0; 
+	count = 0; 
+	updateDisplay(); // Update the display to reflect reset
+}); 
+
+function stopWatch() { 
+	if (timer) { 
+		count++; 
+
+		if (count == 100) { 
+			second++; 
+			count = 0; 
+		} 
+
+		if (second == 60) { 
+			minute++; 
+			second = 0; 
+		} 
+
+		if (minute == 60) { 
+			hour++; 
+			minute = 0; 
+			second = 0; 
+		} 
+
+		updateDisplay(); // Update the display
+
+		setTimeout(stopWatch, 10); // Continue updating
+	} 
+}
+
+function updateDisplay() {
+	let hrString = hour < 10 ? "0" + hour : hour; 
+	let minString = minute < 10 ? "0" + minute : minute; 
+	let secString = second < 10 ? "0" + second : second; 
+	let countString = count < 10 ? "0" + count : count; 
+
+	document.getElementById('hr').innerHTML = hrString; 
+	document.getElementById('min').innerHTML = minString; 
+	document.getElementById('sec').innerHTML = secString; 
+	document.getElementById('count').innerHTML = countString; 
+}
